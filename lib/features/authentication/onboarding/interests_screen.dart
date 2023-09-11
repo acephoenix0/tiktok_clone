5cf379ef340c4faaf3f80c339dc8d09731762387
acephoenix0 <acephoenix0@gmail.com>
2023-09-12 06:43:39 +0900

    Scroll Animations

--- lib/features/authentication/onboarding/interests_screen.dart
+++ lib/features/authentication/onboarding/interests_screen.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/onboarding/widgets/interest_button.dart';
 
-class InterestsScreen extends StatelessWidget {
+class InterestsScreen extends StatefulWidget {
   const InterestsScreen({super.key});
 
   static const interests = [
@@ -47,83 +49,106 @@ class InterestsScreen extends StatelessWidget {
   ];
 
   @override
+  State<InterestsScreen> createState() => _InterestsScreenState();
+}
+
+class _InterestsScreenState extends State<InterestsScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  bool _showTitle = false;
+
+  void _onScroll() {
+    if (_scrollController.offset > 110) {
+      if (_showTitle) return;
+      setState(() {
+        _showTitle = true;
+      });
+    } else {
+      setState(() {
+        _showTitle = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Choose your interests')),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(
-              left: Sizes.size24, right: Sizes.size24, bottom: Sizes.size16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Gaps.v32,
-              const Text(
-                'Choose your interests',
-                style: TextStyle(
-                  fontSize: Sizes.size36,
-                  fontWeight: FontWeight.w700,
+    return Scrollbar(
+      controller: _scrollController,
+      child: Scaffold(
+        appBar: AppBar(
+            title: AnimatedOpacity(
+                opacity: _showTitle ? 1 : 0,
+                duration: const Duration(milliseconds: 500),
+                child: const Text('Choose your interests'))),
+        body: SingleChildScrollView(
+          controller: _scrollController,
+          child: Padding(
+            padding: const EdgeInsets.only(
+                left: Sizes.size24, right: Sizes.size24, bottom: Sizes.size16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Gaps.v32,
+                const Text(
+                  'Choose your interests',
+                  style: TextStyle(
+                    fontSize: Sizes.size36,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-              Gaps.v20,
-              const Text(
-                'Get better video recommendations',
-                style: TextStyle(
-                  fontSize: Sizes.size20,
-                ), 
-              ),
-              Gaps.v64,
-              Wrap(
-                runSpacing: 15,
-                spacing: 15,
-                children: [
-                  for (var interest in interests)
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: Sizes.size16, horizontal: Sizes.size24),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          border:
-                              Border.all(color: Colors.black.withOpacity(0.1)),
-                          borderRadius: BorderRadius.circular(Sizes.size32),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.05),
-                              blurRadius: 5,
-                              spreadRadius: 5,
-                            ),
-                          ]),
-                      child: Text(
-                        interest,
-                        style: const TextStyle(fontWeight: FontWeight.w700),
-                      ),
-                    ),
-                ],
-              )
-            ],
+                Gaps.v20,
+                const Text(
+                  'Get better video recommendations',
+                  style: TextStyle(
+                    fontSize: Sizes.size20,
+                  ),
+                ),
+                Gaps.v64,
+                Wrap(
+                  runSpacing: 15,
+                  spacing: 15,
+                  children: [
+                    for (var interest in InterestsScreen.interests)
+                      InterestButton(interest: interest),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        elevation: 2,
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: Sizes.size16,
-            bottom: Sizes.size40,
-            left: Sizes.size24,
-            right: Sizes.size24,
-          ),
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: Sizes.size20,
+        bottomNavigationBar: BottomAppBar(
+          elevation: 2,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: Sizes.size16,
+              bottom: Sizes.size40,
+              left: Sizes.size24,
+              right: Sizes.size24,
             ),
-            decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-            child: const Text(
-              'Next',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: Sizes.size16,
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: Sizes.size20,
+              ),
+              decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+              child: const Text(
+                'Next',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: Sizes.size16,
+                ),
               ),
             ),
           ),
