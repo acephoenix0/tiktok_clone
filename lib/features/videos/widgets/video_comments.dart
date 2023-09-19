103a4c676beebe43ac35589cd853fe5563a8a3b3
acephoenix0 <acephoenix0@gmail.com>
2023-09-19 12:04:27 +0900

    Light Navigation

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -13,6 +13,8 @@ class VideoComments extends StatefulWidget {
 class _VideoCommentsState extends State<VideoComments> {
   bool _isWriting = false;
 
+  final ScrollController _scrollController = ScrollController();
+
   void _onClosePressed() {
     Navigator.of(context).pop();
   }
@@ -59,57 +61,65 @@ class _VideoCommentsState extends State<VideoComments> {
           onTap: _stopWriting,
           child: Stack(
             children: [
-              ListView.separated(
-                padding: const EdgeInsets.symmetric(
-                    vertical: Sizes.size10, horizontal: Sizes.size16),
-                itemBuilder: (context, index) {
-                  return Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const CircleAvatar(
-                        radius: 18,
-                        child: Text('웅'),
-                      ),
-                      Gaps.h10,
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+              Scrollbar(
+                controller: _scrollController,
+                child: ListView.separated(
+                  controller: _scrollController,
+                  padding: const EdgeInsets.only(
+                    top: Sizes.size10,
+                    bottom: Sizes.size96,
+                    left: Sizes.size16,
+                    right: Sizes.size16,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const CircleAvatar(
+                          radius: 18,
+                          child: Text('웅'),
+                        ),
+                        Gaps.h10,
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '웅',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: Sizes.size14,
+                                    color: Colors.grey.shade500),
+                              ),
+                              Gaps.v3,
+                              const Text(
+                                  'That\'s not it I\'ve seen the same thing but also in a cave.')
+                            ],
+                          ),
+                        ),
+                        Gaps.h10,
+                        Column(
                           children: [
-                            Text(
-                              '웅',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: Sizes.size14,
-                                  color: Colors.grey.shade500),
+                            FaIcon(
+                              FontAwesomeIcons.heart,
+                              color: Colors.grey.shade500,
+                              size: Sizes.size20,
                             ),
-                            Gaps.v3,
-                            const Text(
-                                'That\'s not it I\'ve seen the same thing but also in a cave.')
+                            Gaps.v2,
+                            Text(
+                              '52.2K',
+                              style: TextStyle(color: Colors.grey.shade500),
+                            ),
                           ],
                         ),
-                      ),
-                      Gaps.h10,
-                      Column(
-                        children: [
-                          FaIcon(
-                            FontAwesomeIcons.heart,
-                            color: Colors.grey.shade500,
-                            size: Sizes.size20,
-                          ),
-                          Gaps.v2,
-                          Text(
-                            '52.2K',
-                            style: TextStyle(color: Colors.grey.shade500),
-                          ),
-                        ],
-                      ),
-                    ],
-                  );
-                },
-                itemCount: 10,
-                separatorBuilder: (BuildContext context, int index) {
-                  return Gaps.v20;
-                },
+                      ],
+                    );
+                  },
+                  itemCount: 10,
+                  separatorBuilder: (BuildContext context, int index) {
+                    return Gaps.v20;
+                  },
+                ),
               ),
               Positioned(
                 bottom: 0,
